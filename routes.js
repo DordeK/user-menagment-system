a7c0ec92308acb6aa9200f5dbaa6a595ab6f5e9a
Dorde <dorde.kremenovic@dlabs.si>
2022-07-09 20:09:40 +0200

    add pagination, filtering, sorting functionality + start working on FE

--- routes.js
+++
@@ -1,7 +0,0 @@
-// let userController = req('./controllers/user');
-import userController from './controllers/user/index.cjs'
-const routes = (app) => {
-    app.use('/user', userController);
-};
-
-export default routes
